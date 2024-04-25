85a2207026fc7e75cd87eaf73ceced30b9dc26e7
raphaelsommer <sommerraphael.rs@gmail.com>
2024-04-25 11:46:08 +0200

    implemented class-version of the timer so that it is instantiatable via the main.py

--- devices/rp2/main.py
+++ devices/rp2/main.py
@@ -2,24 +2,28 @@ import time
 import paho.mqtt.client as mqtt
 from paho.mqtt.properties import Properties
 from paho.mqtt.packettypes import PacketTypes
-import ssl
 import threading
 from midi_ip_game import MidiIpGame
+from general_timer import Timer
 
 # MQTT Konfigurationen
 MQTT_BROKER = "192.168.0.102"  # Beispiel-Broker, ersetze diesen durch deinen Broker
 MQTT_PORT = 1883
 MQTT_TRANSPORT_PROTOCOL = "tcp"
 
+# Topics for General and Timer
 MQTT_TOPIC_GEN_START = "/gen/start"
 MQTT_TOPIC_GEN_STOP = "/gen/stop"
+# Topics for the MIDI-IP-Game and RFID
 MQTT_TOPIC_DOOR_A5 = "/door/a5"
 MQTT_TOPIC_C1_RFID = "/c1/rfid"
 
+# Flags for the MIDI-IP-Game and Timer
 isStartMIDIIPGame = False
 isStartTimer = False
 
-# MQTT Methods
+
+### MQTT Methods
 def on_message(client, userdata, msg):
     global isStartMIDIIPGame, isStartTimer
     # print(msg.topic + " " + str(msg.payload))
@@ -31,31 +35,27 @@ def on_message(client, userdata, msg):
         isStartMIDIIPGame = True
 
 
-
-# Client Setup
+##### Client Setup
 client = mqtt.Client(client_id="rp3", protocol=mqtt.MQTTv5, callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
 client.username_pw_set(username="rp3_sub", password="rp3arose1234!")
-###client.on_connect = on_connect
+'''client.on_connect = on_connect'''
 client.on_message = on_message
-
 properties = Properties(PacketTypes.CONNECT)
 client.connect(MQTT_BROKER, MQTT_PORT, keepalive=60)
 client.loop_start()  # Starte den MQTT-Client im Hintergrund
-###client.publish("/test", "Test 2")
 client.subscribe([(MQTT_TOPIC_GEN_START, 2), (MQTT_TOPIC_GEN_STOP, 2), (MQTT_TOPIC_DOOR_A5, 2)])
 
 MIDIIpGame = MidiIpGame()
+MainTimer = Timer()
 
-# MAIN LOOP
-stop = False
-
+thread1 = threading.Thread(target=MainTimer.startTimer)
 thread2 = threading.Thread(target=MIDIIpGame.startGame)
-#thread1 = threading.Thread(target=Timer.startTimer)
+
+stop = False
 while not stop:
     if isStartTimer:
         print("Starting Timer")
-        #Timer = Timer()
-        #thread1.start()
+        thread1.start()
     if isStartMIDIIPGame:
         print("Starting MIDI-IP-Game")
         thread2.start()
@@ -64,9 +64,12 @@ while not stop:
         print("Stopping MIDI-IP-Game")
         MIDIIpGame.stopGame()
         client.publish(topic=MQTT_TOPIC_C1_RFID, payload="1", qos=2)
+    if MainTimer.getFinished():
+        print("Stopping Timer")
+        client.publish(topic=MQTT_TOPIC_GEN_STOP, payload="1", qos=2)
 
     if thread2.is_alive():
-        #thread1.join()
+        thread1.join()
         thread2.join()
 
 client.disconnect()
