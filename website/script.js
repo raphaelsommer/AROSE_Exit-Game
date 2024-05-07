38414a047b76be43c6931a54bed98123267f182b
Raphael Sommer <sommerraphael.rs@gmail.com>
2024-05-07 11:25:26 +0200

    repaired content unfold function

--- website/script.js
+++ website/script.js
@@ -1,10 +1,13 @@
 function revealContent() {
     var secretCode = document.getElementById("secretCode").value;
     var extraContent = document.getElementById("extraContent");
+    //var input = document.getElementsById("input");
 
-    if (secretCode === "1234") { // Replace "1234" with your specific code
-        extraContent.classList.remove("hidden");
-        extraContent.classList.add("visible");
+    if (secretCode === "1234") { 
+        extraContent.classList.remove("Spielanleitung_hiddenContent");
+        extraContent.classList.add("Spielanleitung_visibleContent");
+        //input.classList.remove("Spielanleitung_Eingabe");
+        //input.classList.add("Spielanleitung_hiddenEingabe");
     } else {
         alert("Wrong code!");
     }
